c3ec21eebbd03c1accc797477d84114a93221d8f
pradeepbasyal2003 <pradeepbasyal2003@gmail.com>
2023-12-07 18:11:28 +0545

    home page bug fixes

--- home/models.py
+++ home/models.py
@@ -17,21 +17,21 @@ class Featured(models.Model):
     def __str__(self):
         return self.name
 
-LABELS = (('' , 'default') , ('trending' , 'trending') , ('most played' , 'most played'))
+LABELS = (('' , 'default') , ('trending' , 'trending') , ('most played' , 'most played') , ('featured' , 'featured'))
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     discounted_price = models.IntegerField(default = 0)
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField(upload_to='media' , blank=True)
     description = models.TextField(blank = True)
     game_id = models.CharField(max_length = 100)
     category = models.ManyToManyField(Category)
-
     tags = models.CharField(max_length = 200)
-    labels = models.CharField(choices = LABELS , max_length=50 , blank = True)
-
+    LABELS = (('', 'default'), ('trending', 'trending'), ('most played', 'most played') , ('featured' , 'featured'))
+    labels = models.CharField(choices=LABELS, max_length=50, blank=True)
     def __str__(self):
         return self.name
+
 
 
 
--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from django.views import View
 from .models import *
@@ -9,7 +11,14 @@ class Base(View):
 class HomeView(Base):
     def get(self, request):
         self.views['categories'] = Category.objects.all()
-        self.views['products'] = Product.objects.filter()
+        trending_products = Product.objects.filter(labels = "trending").all()
+        self.views['trending'] = random.sample(list(trending_products),4)
+        mostplayed_products = Product.objects.filter(labels = "most played").all()
+        self.views["most_played"] = random.sample(list(mostplayed_products),6)
+        featured = Product.objects.filter(labels = "featured").all()
+        self.views['featured'] =random.sample(list(featured) , 1)
+
+
         return render(request , 'index.html',self.views)
 
 
